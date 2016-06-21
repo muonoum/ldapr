e2583e20196bf7e0300e17d9d3cf819786eed0a1
Alexander Stensrud <alex@stensrud.nu>
2016-06-21 11:56:20 +0200

    Rewrite, add template support

--- main.go
+++ main.go
@@ -5,60 +5,80 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"text/template"
 
 	"github.com/go-ldap/ldap"
+	"github.com/spektroskop/util"
 	"github.com/spf13/viper"
 )
 
 func main() {
 	flag.Usage = func() {
-		fmt.Println("ldapr <search> [attributes]")
+		fmt.Println(`ldapr -term "TERM" [-attr NAME,...] -tmpl "TEMPLATE"`)
 		os.Exit(2)
 	}
 
+	term := flag.String("term", "", "")
+	attr := flag.String("attr", "", "")
+	tmpl := flag.String("tmpl", "", "")
+
 	flag.Parse()
 
-	viper.SetConfigName("config")
+	viper.SetConfigName("ldapr")
 	viper.SetConfigType("toml")
 
 	viper.AddConfigPath(".")
+	viper.AddConfigPath("~")
 
 	if err := viper.ReadInConfig(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		util.Error(err, 1)
 	}
 
 	conn, err := ldap.Dial("tcp", viper.GetString("server"))
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		util.Error(err, 1)
 	}
 
 	if err := conn.Bind(viper.GetString("user"), viper.GetString("password")); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		util.Error(err, 1)
 	}
 
-	args := flag.Args()
-	if len(args) < 1 {
-		flag.Usage()
-	}
-
-	search := flag.Args()[0]
 	var attrs []string
-	if len(args) > 1 {
-		attrs = strings.Split(args[1], ",")
+	if *attr != "" {
+		attrs = strings.Split(*attr, ",")
 	}
 
 	if res, err := conn.SearchWithPaging(ldap.NewSearchRequest(viper.GetString("base"),
 		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
-		search, attrs, []ldap.Control{},
+		*term, attrs, []ldap.Control{},
 	), 1000); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		util.Error(err, 1)
 	} else {
-		for _, entry := range res.Entries {
-			entry.PrettyPrint(0)
+		if *tmpl != "" {
+			for _, entry := range res.Entries {
+				m := map[string]string{
+					"DN": entry.DN,
+				}
+
+				for _, attr := range entry.Attributes {
+					m[attr.Name] = strings.Join(attr.Values, ",")
+				}
+
+				if *tmpl != "" {
+					tmp, err := template.New("ldapr").Parse(strings.TrimSpace(*tmpl) + "\n")
+					if err != nil {
+						util.Error(err, 1)
+					}
+					if err := tmp.Execute(os.Stdout, m); err != nil {
+						util.Error(err, 1)
+					}
+				}
+			}
+		} else {
+			for _, entry := range res.Entries {
+				entry.PrettyPrint(0)
+			}
 		}
+
 	}
 }
