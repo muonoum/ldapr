314583d7b3a02342c8e6200e5f84a14559efc5b5
Alexander Stensrud <alexander.stensrud@sparebank1.no>
2024-10-30 14:59:30 +0100

    Parameter for configfil og litt opprydding

---
+++ go.mod
@@ -0,0 +1,17 @@
+module ldapr
+
+go 1.23.2
+
+require (
+	github.com/go-ldap/ldap/v3 v3.4.8
+	github.com/pelletier/go-toml/v2 v2.2.3
+)
+
+require (
+	github.com/Azure/go-ntlmssp v0.0.0-20221128193559-754e69321358 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/go-asn1-ber/asn1-ber v1.5.5 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,46 +1,70 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"os"
 	"strings"
 	"text/template"
 
-	"github.com/go-ldap/ldap"
-	"github.com/spektroskop/util"
-	"github.com/spf13/viper"
+	"github.com/go-ldap/ldap/v3"
+	"github.com/pelletier/go-toml/v2"
 )
+
+type Config struct {
+	Server   string
+	User     string
+	Password string
+	Base     string
+}
+
+func configure(path string) (Config, error) {
+	var config Config
+	f, err := os.Open(path)
+	if err != nil {
+		return config, err
+	}
+	err = toml.NewDecoder(f).Decode(&config)
+	return config, err
+}
 
 func main() {
 	flag.Usage = func() {
-		fmt.Println(`ldapr -term "TERM" [-attr NAME,...] -tmpl "TEMPLATE"`)
+		fmt.Println(`ldapr [-config PATH] -filter "TERM" [-attributes NAME,...] -template "TEMPLATE"`)
 		os.Exit(2)
 	}
 
 	term := flag.String("term", "", "")
 	attr := flag.String("attr", "", "")
 	tmpl := flag.String("tmpl", "", "")
+	cf := flag.String("config", "/etc/ldapr.toml", "")
 
 	flag.Parse()
 
-	viper.SetConfigName("ldapr")
-	viper.SetConfigType("toml")
-
-	viper.AddConfigPath(".")
-	viper.AddConfigPath("~")
-
-	if err := viper.ReadInConfig(); err != nil {
-		util.Error(err, 1)
-	}
-
-	conn, err := ldap.Dial("tcp", viper.GetString("server"))
+	config, err := configure(*cf)
 	if err != nil {
-		util.Error(err, 1)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	if err := conn.Bind(viper.GetString("user"), viper.GetString("password")); err != nil {
-		util.Error(err, 1)
+	var conn *ldap.Conn
+
+	if strings.HasSuffix(config.Server, ":636") {
+		if conn, err = ldap.DialTLS("tcp", config.Server, &tls.Config{}); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	} else {
+		if conn, err = ldap.Dial("tcp", config.Server); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	if err := conn.Bind(config.User, config.Password); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
 	var attrs []string
@@ -48,11 +72,12 @@ func main() {
 		attrs = strings.Split(*attr, ",")
 	}
 
-	if res, err := conn.SearchWithPaging(ldap.NewSearchRequest(viper.GetString("base"),
+	if res, err := conn.SearchWithPaging(ldap.NewSearchRequest(config.Base,
 		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
 		*term, attrs, []ldap.Control{},
 	), 1000); err != nil {
-		util.Error(err, 1)
+		fmt.Println(err)
+		os.Exit(1)
 	} else {
 		if *tmpl != "" {
 			for _, entry := range res.Entries {
@@ -67,10 +92,12 @@ func main() {
 				if *tmpl != "" {
 					tmp, err := template.New("ldapr").Parse(strings.TrimSpace(*tmpl) + "\n")
 					if err != nil {
-						util.Error(err, 1)
+						fmt.Println(err)
+						os.Exit(1)
 					}
 					if err := tmp.Execute(os.Stdout, m); err != nil {
-						util.Error(err, 1)
+						fmt.Println(err)
+						os.Exit(1)
 					}
 				}
 			}
